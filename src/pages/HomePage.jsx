f47bded3c2031d977229a1411ad859b0b63ef037
Aliia <101452567+ceo-of-albeni@users.noreply.github.com>
2022-11-16 20:09:34 +0600

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,23 +2,21 @@ import React, { useState } from "react";
 import "../styles/HomePage.css";
 import Footer from "../components/Footer/Footer";
 import GameList from "../components/GamesList";
+import { Swiper, SwiperSlide } from "swiper/react";
+import App from "./Slider";
 
 const HomePage = () => {
   const [page, setPage] = useState(1);
   return (
     <div>
+      <App />
       <div className="home_top">
-        <img
-          src="https://images.hdqwalls.com/wallpapers/overwatch-2-key-art-4k-ah.jpg"
-          width="100%"
-        />
-        <h1>Games</h1>
-      </div>
-
+      
+      <h1>Games</h1>
+    </div>
       <div>
         <GameList page={page} setPage={setPage} />
       </div>
-      {/* <Footer /> */}
     </div>
   );
 };
