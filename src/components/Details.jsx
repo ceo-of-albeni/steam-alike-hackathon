84cb7eea35a8921ef2790c6351b28d994cb5057d
Aliia <101452567+ceo-of-albeni@users.noreply.github.com>
2022-11-16 20:08:23 +0600

    Update Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -25,25 +25,58 @@ const Details = () => {
       }}>
       <Card
         style={{ margin: "10px", display: "flex", flexDirection: "row" }}
-        sx={{ maxWidth: 800 }}>
+        sx={{ maxWidth: 900 }}>
         <CardMedia
           component="img"
           alt="error"
           height="450"
           image={oneGame.img}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Title:{oneGame.title}
+        <CardContent
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+          }}>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            style={{
+              color: "midnightblue",
+              fontWeight: "bold",
+              fontSize: "35px",
+            }}>
+            {oneGame.title}
           </Typography>
-          <Typography gutterBottom variant="h5" component="div">
-            Price:{oneGame.price}
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            style={{
+              paddingLeft: "1%",
+              fontSize: "23px",
+              fontWeight: "lighter",
+              color: "grey",
+            }}>
+            {oneGame.description}
           </Typography>
-          <Typography gutterBottom variant="h5" component="div">
-            Type:{oneGame.type}
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            style={{ fontSize: "20px", fontWeight: "light" }}>
+            {oneGame.type}
           </Typography>
-          <Typography gutterBottom variant="h5" component="div">
-            Description:{oneGame.description}
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            style={{
+              fontSize: "30px",
+              fontWeight: "bold",
+            }}>
+            ${oneGame.price}
           </Typography>
         </CardContent>
       </Card>
